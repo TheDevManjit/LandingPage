c992336b3566930f797264de16404f9968479b32
TheDevManjit <majeetpaswan77@gmail.com>
2025-03-31 07:29:26 +0530

    3.1.2 (Loader) update

--- Client/src/components/Loder.jsx
+++ Client/src/components/Loder.jsx
@@ -9,8 +9,8 @@ const Loader = () => {
         Welcome to TeachMark
       </p>
       <div className="relative w-[120px] h-[90px] mt-8">
-        <div className="absolute bottom-[30px] left-[50px] w-[30px] h-[30px] bg-blue-500 rounded-full animate-bounce"></div>
-        <div className="absolute right-0 top-0 h-[7px] w-[45px] rounded-md shadow-[0_5px_0_#f2f2f2,-35px_50px_0_#f2f2f2,-70px_95px_0_#f2f2f2] animate-[loading-step_1s_ease-in-out_infinite]"></div>
+    
+      <div class="loader"></div>
       </div>
     </div>
   );
